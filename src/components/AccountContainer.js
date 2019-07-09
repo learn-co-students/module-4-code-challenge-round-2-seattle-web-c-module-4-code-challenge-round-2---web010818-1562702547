f6dbe30321b49cdfa10efb4b9ad730a4b9a8c9f2
github username <github email address>
2019-07-09 14:41:27 -0700

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,25 +5,51 @@ import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+  state = {
+    textInput: '',
+    list: []
+  }
+
+  componentDidMount(){
+  	fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+  	  .then(resp => resp.json())
+  	  .then(list => {
+  	  	this.setState({list})
+  	  })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+}
+
+  handleChange = (event, textInput) => {
+    console.log(textInput)
+
+    event.preventDefault()
+
+{/*tried using event target value*/}
+
+    const currentState = [...this.state.list]
+    const newState = currentState.filter(list => list.id === textInput.id)
+
+    this.setState({
+      textInput: newState
+    })
+
+  }
 
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+    // then replace the default transactions with a call to the AP
 
-  }
-
-  handleChange(event) {
-    // your code here
-  }
 
   render() {
 
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search list={this.state.list} handleSubmit={this.handeSubmit} handleChange={this.handleChange} textInput={this.state.textInput} />
+        <TransactionsList textInput={this.state.textInput} list={this.state.list}/>
       </div>
     )
   }
